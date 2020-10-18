abd0722f96c102b304628cd0ad0c10ce9f44403f
Guilherme Sardinha <71841173+Guilherme-Sardinha@users.noreply.github.com>
2020-10-18 18:45:16 -0300

    Create Banco.py

---
+++ Banco.py
@@ -0,0 +1,7 @@
+Nome = input("Digite o nome do cliente: ")
+Dia = input("Digite o dia de vencimento: ")
+Mes = input("Digite o meês de vencimento: ")
+Valor = input("Digite o valor da fatura: ")
+
+print("Olá, ", Nome)
+print("A sua fatura convencimento em ", Dia, " de ", Mes, " no valor de R$ ",Valor, " está fechada.")

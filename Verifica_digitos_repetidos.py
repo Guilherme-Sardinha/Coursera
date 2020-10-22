f1356f000fd3ffbeb16a16f1585f06937ef32d61
Guilherme-Sardinha <guilherme.sardinha@live.com>
2020-10-21 23:55:58 -0300

    vai

--- Verifica_digitos_repetidos.py
+++ Verifica_digitos_repetidos.py
@@ -4,18 +4,21 @@ cont = len(str(valor))
 mostra = 0
 mostra2 = 0
 cont2 = 0
-while 0 != cont:
-    cont = cont - 1
-    mostra = valor
-    mostra = valor % 10
-    mostra2 = valor
-    #deixei a variavel 'mostra2' um passo a frente para realizar a comparação entre os digitos
-    mostra2 = (valor // 10) % 10
-    valor = valor // 10
-    #coloquei um segundo contador para contar o número de vezes que um digito se repetia
-    if mostra == mostra2:
-        cont2 = cont2 + 1
-if cont2 >= 1 and cont != 1:
-    print("sim")
+if valor != 0:
+    while 0 != cont:
+        cont = cont - 1
+        mostra = valor
+        mostra = valor % 10
+        mostra2 = valor
+        #deixei a variavel 'mostra2' um passo a frente para realizar a comparação entre os digitos
+        mostra2 = (valor // 10) % 10
+        valor = valor // 10
+        #coloquei um segundo contador para contar o número de vezes que um digito se repetia
+        if mostra == mostra2:
+            cont2 = cont2 + 1
+    if cont2 >= 1 and cont != 1:
+        print("sim")
+    else:
+        print("não")
 else:
     print("não")
